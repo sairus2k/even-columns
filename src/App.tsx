ef333d41aa90a62189b4a6098775994a7d9631e5
Anton Savoskin <sairus2k@gmail.com>
2019-12-14 17:27:22 +0700

    feat: split text into 3 columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import React, { FC } from 'react';
 import './App.css';
+import { text as initialText } from './mock';
+
+const NUMBER_OF_COLUMNS = 3;
 
 export const App: FC = () => {
-  return <div className="App">hello world</div>;
+  let text = initialText;
+  const quotient = Math.round(text.length / 3);
+  const columns = Array.from({ length: NUMBER_OF_COLUMNS }, (_c, index) => {
+    if (index + 1 === NUMBER_OF_COLUMNS) {
+      return text;
+    }
+    const lastIndex = text.lastIndexOf(' ', quotient);
+    const columnText = text.substring(0, lastIndex);
+    text = text.substring(lastIndex + 1);
+    return columnText;
+  });
+
+  return (
+    <div className="App">
+      {columns.map(column => (
+        <p key={column.substring(0, 10)} contentEditable>
+          {column}
+        </p>
+      ))}
+    </div>
+  );
 };
