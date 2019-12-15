ef9c57b110e0521681667f0ca5432a52b280bf89
Anton Savoskin <sairus2k@gmail.com>
2019-12-15 23:00:55 +0700

    Updates

---
+++ precache-manifest.f1f3ca46022de981c95169874c1d5402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121e53a7c2a42803c959fbf31f1b4db0",
+    "url": "/even-columns/index.html"
+  },
+  {
+    "revision": "a31a561fc76f2b4476f3",
+    "url": "/even-columns/static/css/main.c604cd7d.chunk.css"
+  },
+  {
+    "revision": "ecd44cdd035ce760f6ff",
+    "url": "/even-columns/static/js/2.3c83d430.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/even-columns/static/js/2.3c83d430.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a31a561fc76f2b4476f3",
+    "url": "/even-columns/static/js/main.47a8a589.chunk.js"
+  },
+  {
+    "revision": "bdeaf5c7e1b0ba87263f",
+    "url": "/even-columns/static/js/runtime-main.efe1f883.js"
+  }
+]);
