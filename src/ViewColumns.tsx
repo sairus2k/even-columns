51ff84403bf1cbf320f173b50aa4477f695d0f1e
Anton Savoskin <sairus2k@gmail.com>
2019-12-15 20:05:17 +0700

    refactor: extract ViewColumns component

---
+++ src/ViewColumns.tsx
@@ -0,0 +1,51 @@
+import React, { FC, useState } from 'react';
+import './ViewColumns.css';
+import { text as initialText } from './mock';
+import { ContentEditable } from './ContentEditable';
+
+const NUMBER_OF_COLUMNS = 3;
+
+const divideTextByColumns = (text: string, columnsAmount: number) => {
+  const quotient = Math.round(text.length / columnsAmount);
+  return Array.from({ length: columnsAmount }, (_c, index) => {
+    if (index + 1 === columnsAmount) {
+      return text;
+    }
+    const lastIndex = text.lastIndexOf(' ', quotient);
+    const columnText = text.substring(0, lastIndex);
+    text = text.substring(lastIndex + 1);
+    return columnText;
+  });
+};
+
+export const ViewColumns: FC = () => {
+  const [actualText, setActualText] = useState(initialText);
+  const [cacheText, setCacheText] = useState(initialText);
+  const texts = divideTextByColumns(actualText, NUMBER_OF_COLUMNS);
+
+  const handleChangeText = (newText: string, index: number) => {
+    const result = [...texts];
+    result[index] = newText;
+    setCacheText(result.join(' '));
+  };
+
+  const handleUpdateText = () => {
+    setActualText(cacheText);
+  };
+
+  return (
+    <>
+      <button onClick={handleUpdateText}>Update</button>
+      <div className="ViewColumns">
+        {texts.map((text, index) => (
+          <ContentEditable
+            key={text.substring(0, 10)}
+            index={index}
+            text={text}
+            onChange={handleChangeText}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
